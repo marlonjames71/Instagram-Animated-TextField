d0e7e0bb454fcfd3a70b0217a815f767b599fc98
Marlon Raskin <marlonr@hey.com>
2023-05-24 00:15:18 -0400

    Initial Commit

---
+++ InstagramAnimatedTextField-Finished/InstagramAnimatedTextField_FinishedApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAnimatedTextField_FinishedApp.swift
+//  InstagramAnimatedTextField-Finished
+//
+//  Created by Marlon Raskin on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAnimatedTextField_FinishedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
